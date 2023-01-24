14dc6dfe494cef59ccfa36e6d088cddb7d916129
Thom Treebus <thomtreebus@gmail.com>
2023-01-24 19:15:33 +0000

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Thom Treebus on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
